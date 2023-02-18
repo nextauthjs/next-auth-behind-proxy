fca52b7d9508010b974aa2c267592e254d84842b
Thang Vu <hi@thvu.dev>
2023-02-18 16:56:42 +0700

    fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,51 +1,23 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import { NextApiRequest, NextApiResponse } from "next"
-// import AppleProvider from "next-auth/providers/apple"
-// import EmailProvider from "next-auth/providers/email"
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export const authOptions: NextAuthOptions = {
-  // https://next-auth.js.org/configuration/providers/oauth
   providers: [
-    /* EmailProvider({
-         server: process.env.EMAIL_SERVER,
-         from: process.env.EMAIL_FROM,
-       }),
-    // Temporarily removing the Apple provider from the demo site as the
-    // callback URL for it needs updating due to Vercel changing domains
-
-    Providers.Apple({
-      clientId: process.env.APPLE_ID,
-      clientSecret: {
-        appleId: process.env.APPLE_ID,
-        teamId: process.env.APPLE_TEAM_ID,
-        privateKey: process.env.APPLE_PRIVATE_KEY,
-        keyId: process.env.APPLE_KEY_ID,
-      },
-    }),
-    */
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  theme: {
-    colorScheme: "light",
-  },
-  callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin"
-      return token
-    },
-  },
 }
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  if (process.env.VERCEL) {
-    // prefer NEXTAUTH_URL, fallback to x-forwarded-host
-    req.headers["x-forwarded-host"] = process.env.NEXTAUTH_URL || req.headers["x-forwarded-host"]
+  if (process.env.NEXTAUTH_URL) {
+    // workaround: Since next-auth ignore NEXTAUTH_URL internally in favor of `x-forwarded-host` if deployed on Vercel,
+    // we need to override the header here for NEXTAUTH_URL to take effect
+    req.headers["x-forwarded-host"] = process.env.NEXTAUTH_URL
   }
-  return NextAuth(req, res, authOptions) // eslint-disable-line new-cap
+  return NextAuth(req, res, authOptions)
 }
