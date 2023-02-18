d5e4959a530c79be738caf6ee21ab5d8d4a942a9
Thang Vu <hi@thvu.dev>
2023-02-18 21:04:33 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -3,5 +3,24 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   trailingSlash: true,
   assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_PREFIX : undefined,
-  basePath: process.env.NEXT_PUBLIC_ASSET_PREFIX
+  basePath: process.env.NEXT_PUBLIC_ASSET_PREFIX,
+  async rewrites() {
+    return [
+      {
+        source: '/authjs/',
+        destination: '/authjs/',
+        has: [
+          {
+            type: 'query',
+            key: 'code',
+          },
+          {
+            type: 'query',
+            key: 'state',
+          },
+        ],
+        permanent: false,
+      },
+    ];
+  },
 }
