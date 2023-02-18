bbb8d6e2b5410322f0b09070e4ca825cfe93d736
Thang Vu <hi@thvu.dev>
2023-02-18 14:00:22 +0700

    fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
 import GithubProvider from "next-auth/providers/github"
-import TwitterProvider from "next-auth/providers/twitter"
-import Auth0Provider from "next-auth/providers/auth0"
+import { NextApiRequest, NextApiResponse } from "next"
 // import AppleProvider from "next-auth/providers/apple"
 // import EmailProvider from "next-auth/providers/email"
 
@@ -45,4 +42,10 @@ export const authOptions: NextAuthOptions = {
   },
 }
 
-export default NextAuth(authOptions)
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  if (process.env.VERCEL) {
+    // prefer NEXTAUTH_URL, fallback to x-forwarded-host
+    req.headers["x-forwarded-host"] = process.env.NEXTAUTH_URL || req.headers["x-forwarded-host"]
+  }
+  return NextAuth(req, res, authOptions) // eslint-disable-line new-cap
+}
