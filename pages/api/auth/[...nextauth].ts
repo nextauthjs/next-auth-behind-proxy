e34e7b65f2b91630d804ee49d64029d3e5b8bba7
Thang Vu <hi@thvu.dev>
2023-03-07 07:28:20 +0700

    fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { NextApiRequest, NextApiResponse } from "next"
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
