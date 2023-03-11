5eb9babc1947287fe8f1f93adfd646afd0c7a3cd
Thang Vu <hi@thvu.dev>
2023-03-11 16:39:28 +0700

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
+import { useRouter } from "next/router";
 import Layout from "../components/layout"
 
 export default function IndexPage() {
+  const router = useRouter();
+  if (router.query.code && router.query.state) router.replace('/', undefined, { shallow: true });
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
+      <h1>NextAuth.js Behind a proxy example</h1>
       <p>
         This is an example site to demonstrate how to use{" "}
-        <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
+        <a href="https://next-auth.js.org">NextAuth.js</a> for authentication when deployed behind a Netlify proxy.
       </p>
     </Layout>
   )
