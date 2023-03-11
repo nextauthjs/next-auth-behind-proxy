91ee6e879908a8c3717357cbebcb240caa5d6a37
Thang Vu <hi@thvu.dev>
2023-03-11 20:22:37 +0700

    fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
-const defaultRedirectUrl = ({ url, baseUrl }: { url: string; baseUrl: string }) => {
-  if (url.startsWith("/")) return `${baseUrl}${url}`
-  else if (new URL(url).origin === baseUrl) return url
-  return baseUrl
-}
-
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export const authOptions: NextAuthOptions = {
@@ -16,24 +10,6 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  callbacks: {
-    async redirect(params) {
-      const defaultRedirect = await defaultRedirectUrl(params)
-
-      const url = new URL(defaultRedirect);
-
-      // Remove the state and code parameters from the URLSearchParams object
-      const queryParams = new URLSearchParams(url.search);
-      queryParams.delete('state');
-      queryParams.delete('code');
-
-      // Update the search property of the URL object with the modified params
-      url.search = queryParams.toString();
-
-      // Get the modified URL string
-      return url.toString();
-    },
-  }
 }
 
 export default NextAuth(authOptions)
